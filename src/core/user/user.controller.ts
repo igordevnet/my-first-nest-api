bff9ce2cabc1b2b74790fe551cef8c7545dae839
IGOR ALMEIDA <igor.almeida55@etec.sp.gov.br>
2026-01-25 18:13:38 -0300

    Last Prisma commit

--- src/core/user/user.controller.ts
+++ src/core/user/user.controller.ts
@@ -1,10 +1,10 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from "@nestjs/common";
+import { BadRequestException, Body, Controller, Delete, FileTypeValidator, Get, MaxFileSizeValidator, Param, ParseFilePipe, ParseIntPipe, Patch, Post, UploadedFile, UploadedFiles, UseGuards, UseInterceptors } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { UpdateUserDTO } from "./dto/update-user.dto";
 import { UserService } from "./user.service";
 import { ParamId } from "src/shared/Decorators/param-id.decorator";
 import { AuthGuard } from "src/shared/auth/guards/auth.guard";
-import { FileFieldsInterceptor, FileInterceptor, FilesInterceptor } from "@nestjs/platform-express";
+import { FileFieldsInterceptor, FileInterceptor } from "@nestjs/platform-express";
 import { User } from "src/shared/Decorators/user.decorator";
 
 @Controller(`users`)
@@ -43,21 +43,36 @@ export class UserController {
     }
 
     @Post('photo')
+    @Post('photo')
     @UseGuards(AuthGuard)
     @UseInterceptors(FileInterceptor('file'))
-    async uploadPhoto(@User() user, @UploadedFile() file: Express.Multer.File) {
+    async uploadPhoto(
+        @User() user,
+        @UploadedFile(new ParseFilePipe({
+            validators: [new MaxFileSizeValidator({maxSize: 1024 * 300})]
+        })) file: Express.Multer.File,
+    ) {
+        if (!file) {
+            throw new BadRequestException('File is required');
+        }
+
+        if (file.mimetype !== 'image/jpeg') {
+            throw new BadRequestException('Only JPEG images are allowed');
+        }
+
         return this.userService.uploadPhoto(user.id_user, file);
     }
 
+
     @Post('files-fields')
     @UseGuards(AuthGuard)
-    @UseInterceptors(FileFieldsInterceptor([{ 
+    @UseInterceptors(FileFieldsInterceptor([{
         name: 'documents',
         maxCount: 10,
     }]))
-    async uploadFiles(@User() user, @UploadedFiles() files: { documents: Express.Multer.File[]}) {
+    async uploadFiles(@User() user, @UploadedFiles() files: { documents: Express.Multer.File[] }) {
         console.log(files)
-        
+
         return this.userService.uploadFiles(user.id_user, files);
     }
 }
